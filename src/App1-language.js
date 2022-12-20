47d469bf785669f60b06b2c1aab9a581dc070b26
TheBedanse <05832012@mail.ru>
2022-12-20 17:42:52 +0500

    Create App1-language.js

---
+++ src/App1-language.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next';
+
+const App = () => {
+
+  const { t, i18n } = useTranslation();
+
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+  return (
+    <>
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <button onClick={() => changeLanguage("fr")}>FR</button>
+      <div>{t("text")}</div>
+      <div>{t("hello")}</div>
+    </>
+  )
+}
+
+export default App
